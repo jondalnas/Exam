06901959c74581fbd4fba26a23325645bed62e60
Jonas Jensen <jdudal11@gmail.com>
2019-12-16 08:56:27 +0100

    Added Vector3 class

---
+++ src/com/JoL/PathTracer/Vector3.java
@@ -0,0 +1,19 @@
+package com.JoL.PathTracer;
+
+public class Vector3 {
+	public double x, y, z;
+	
+	public Vector3(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public Vector3 add(Vector3 v) {
+		return new Vector3(x + v.x, y + v.y, z + v.z);
+	}
+	
+	public Vector3 mult(Vector3 v) {
+		return new Vector3(x * v.x, y * v.y, z * v.z);
+	}
+}
