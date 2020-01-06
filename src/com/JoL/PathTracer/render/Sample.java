fe48eb35aeff491e3b17366ca7fc542180c04f52
Jonas Jensen <jdudal11@gmail.com>
2020-01-06 14:24:28 +0100

    Added sin and cos lookup tables

--- src/com/JoL/PathTracer/render/Sample.java
+++ src/com/JoL/PathTracer/render/Sample.java
@@ -6,8 +6,10 @@ public class Sample {
 	private final int width, height;
 	public Pixel[] screen;
 
-	public static final double FOV = Math.toRadians(60);
+	public static final double FOV = 60;
 	public static double aspect, yFOV;
+	
+	public static double[] sin, cos;
 	
 	public Sample(int width, int height) {
 		this.width = width;
@@ -21,22 +23,36 @@ public class Sample {
 		for (int i = 0; i < screen.length; i++) {
 			screen[i] = new Pixel(0);
 		}
+		
+		//Init sin and cos
+		if (sin == null) {
+			sin = new double[width];
+			cos = new double[width];
+			
+			for (int x = 0; x < width; x++) {
+				double xAngle = ((double) x / width - 0.5) * Math.toRadians(FOV);
+
+				sin[x] = Math.sin(xAngle);
+				cos[x] = Math.cos(xAngle);
+			}
+		}
 	}
 	
 	public void render() {
 		for (int y = 0; y < height; y++) {
-			double yAngle = (0.5 - (double) y / height) * yFOV;
+			//It's "0.5 -" instead of "- 0.5" because we are flipping the y-axis 
+			double yAngle = (0.5 - (double) y / height) * Math.toRadians(yFOV);
 			double tanY = Math.tan(yAngle);
-			double size = 1.0 / Math.sqrt(1 + tanY*tanY);
+			//We know that the xz components have a length of 1, so we only need to calculate the length of the tan component
+			double scale = 1.0 / Math.sqrt(1 + tanY*tanY);
 			
 			for (int x = 0; x < width; x++) {
-				double xAngle = ((double) x / width - 0.5) * FOV;
-				
-				Vector3 dir = new Vector3(Math.sin(xAngle), tanY, Math.cos(xAngle));
+				Vector3 dir = new Vector3(sin[x], tanY, cos[x]);
 				
 				//Normalize
-				dir.multEqual(size);
+				dir.multEqual(scale);
 				
+				//Drawing the directions
 				screen[x+y*width].setColor(dir.add(new Vector3(1, 1, 1)).mult(0.5));
 			}
 		}
