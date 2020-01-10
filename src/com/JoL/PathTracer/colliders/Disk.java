d877ccae5fd36afb624ea83cd22b8f42fc6d5c7a
lenehc <lene.hojberg.christensen@gmail.com>
2020-01-10 09:39:53 +0100

    Added disk and performed unit test

---
+++ src/com/JoL/PathTracer/colliders/Disk.java
@@ -0,0 +1,21 @@
+package com.JoL.PathTracer.colliders;
+
+import com.JoL.PathTracer.Vector3;
+
+public class Disk extends Plane {
+	private double r;
+
+	public Disk(Vector3 pos, Vector3 normal, double radius) {
+		super(pos, normal);
+		r = radius;
+	}
+	
+	public Hit collides(Ray ray) {
+		Hit hit = super.collides(ray);
+		Vector3 toVec = pos.minus(hit.pos);
+		double dist = toVec.dot(toVec);
+		if (dist < r*r) return hit;
+		
+		return null;
+	}
+}
